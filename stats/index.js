6c62319c67001fde6306b38b3ef0038f1154c663
John Rapp <johnfarnes@hotmail.com>
2017-02-11 22:07:04 +0100

    Added pi containers

--- stats/index.js
+++ stats/index.js
@@ -2,15 +2,35 @@ const dbConnect = require('./db.js');
 const schedule = require('./schedule.js');
 const sample = require('./sample.js');
 
-dbConnect(function(err, db) {
+let db;
+dbConnect(function(err, _db) {
 	if (err) {
 		return console.error('Mongo error', err);
 	}
 	console.log('Connected to Mongo');
+	db = _db;
 
 	schedule(function() {
 		// console.log('test')
 		sample(db);
 	});
 
+});
+
+// TODO remove
+const express = require('express');
+const port = 8080;
+const app = express();
+
+app.listen(port, function () {
+	console.log(`Server listening on ${port}`);
+});
+
+app.get('/', (req, res) => {
+	db.laundrySamples.find({}).toArray(function(err, docs) {
+		if (err) {
+			return res.send(err);
+		}
+		res.send(docs);
+	});
 });
--- stats/schedule.js
+++ stats/schedule.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule');
 
 module.exports = function(jobFunction) {
-  const job = schedule.scheduleJob('0,30 * * * *', jobFunction);
-  // const job = schedule.scheduleJob('*/10 * * * * *', jobFunction);
+  // const job = schedule.scheduleJob('0,30 * * * *', jobFunction);
+  const job = schedule.scheduleJob('*/10 * * * * *', jobFunction);
 
 };
